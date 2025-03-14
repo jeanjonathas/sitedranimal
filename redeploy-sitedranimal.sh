2f1f136692bd10659d50602694424514e1e70cb9
Jean <jeanjonathasfb@gmail.com>
2025-03-14 20:30:24 -0300

    oasdf

--- redeploy-sitedranimal.sh
+++ redeploy-sitedranimal.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Criar diretório de logs se não existir
-mkdir -p /var/log
-
 # Log de início
 echo "Iniciando reimplantação do site Dr. Animal em $(date)" >> /var/log/webhook-redeploy.log
 
